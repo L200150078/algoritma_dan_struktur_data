53a2c1366264718f4e020b869f76b6b136053b5a
L200150078 <muchamadthoyib@gmail.com>
2017-06-19 13:36:47 +0700

    Add files via upload
    Tugas 2 algoritma dan struktur data

---
+++ tugas02-a.py
@@ -0,0 +1,10 @@
+
+print("%27s" %("Tabel KODE ASSCII")) 
+print("---------------------------------------")
+
+## Nilai variabel kode akan berubah secara otomatis mulai dari 32 sampai 126
+
+print("| %2s | %3s | %9s   | %6s |" %( "Simbol", "Kode","Binary" ,"Hex")) ##supaya tertata rapi
+for kode in range(32,126):
+    print("|%4c    | %4d | %10s  | %6s |" %( kode, kode, bin(kode), hex(kode) ) )
+print("----------------------------------------")
